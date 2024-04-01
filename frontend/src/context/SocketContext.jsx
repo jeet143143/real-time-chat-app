0e9fa9c6551138c9dd38ca3245ad022089c81b6f
jeet143143 <senapatijeet2004@gmail.com>
2024-04-02 01:49:19 +0530

    third-commit

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://real-time-chat-app-1-kyce.onrender.com/", {
+			const socket = io("http://localhost:5000", {
 				query: {
 					userId: authUser._id,
 				},

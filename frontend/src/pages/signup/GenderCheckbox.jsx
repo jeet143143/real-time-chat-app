716ab4103ae226249221e7e076db5696298b8409
jeet143143 <senapatijeet2004@gmail.com>
2024-04-03 00:13:49 +0530

    comments

--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -28,31 +28,3 @@ const GenderCheckbox = ({ onCheckboxChange, selectedGender }) => {
 };
 export default GenderCheckbox;
 
-// STARTER CODE FOR THIS FILE
-// const GenderCheckbox = () => {
-// 	return (
-// 		<div className='flex'>
-// 			<div className='form-control'>
-// 				<label className={`label gap-2 cursor-pointer`}>
-// 					<span className='label-text'>Male</span>
-// 					<input type='checkbox' className='checkbox border-slate-900' />
-// 				</label>
-// 			</div>
-// 			<div className='form-control'>
-// 				<label className={`label gap-2 cursor-pointer`}>
-// 					<span className='label-text'>Female</span>
-// 					<input type='checkbox' className='checkbox border-slate-900' />
-// 				</label>
-// 			</div>
-// 		</div>
-// 	);
-// };
-// export default GenderCheckbox;
-
-
-{/* <div className="form-control">
-  <label className="label cursor-pointer">
-    <span className="label-text">Remember me</span> 
-    <input type="checkbox" defaultChecked className="checkbox" />
-  </label>
-</div> */}
